532f62c0ab177e56697740cc042d9d9765a562ba
arunpanigrahi93 <arunpanigrahi94@gmail.com>
2024-08-31 22:37:33 +0530

    updated carousal sizes of image

--- frontend/src/components/ProductCarousel.jsx
+++ frontend/src/components/ProductCarousel.jsx
@@ -12,14 +12,27 @@ const ProductCarousel = () => {
     <Carousel pause="hover" className="bg-primary mb-4">
       {products.map((product) => (
         <Carousel.Item key={product._id}>
-          <Link to={`/product/${product._id}`}>
-            <Image src={product.image} alt={product.name} fluid />
-            <Carousel.Caption className="carousel-caption">
-              <h2 className="text-white text-right">
-                {product.name} (${product.price})
-              </h2>
-            </Carousel.Caption>
-          </Link>
+          <div className="carousel-content">
+            <div className="carousel-image-wrapper">
+              <Image
+                src={product.image}
+                alt={product.name}
+                className="carousel-image"
+              />
+            </div>
+            <div className="carousel-text-wrapper">
+              <Link
+                to={`/product/${product._id}`}
+                className="text-decoration-none"
+              >
+                <Carousel.Caption className="carousel-caption">
+                  <h2 className="text-white text-right">
+                    {product.name} (${product.price})
+                  </h2>
+                </Carousel.Caption>
+              </Link>
+            </div>
+          </div>
         </Carousel.Item>
       ))}
     </Carousel>
