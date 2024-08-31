10c9cc7a7d176780014ba17680b6438a4cdf7b4c
arunpanigrahi93 <arunpanigrahi94@gmail.com>
2024-08-31 21:43:26 +0530

    implement lazy loading to home screen

--- frontend/src/components/screens/HomeScreen.jsx
+++ frontend/src/components/screens/HomeScreen.jsx
@@ -1,12 +1,14 @@
+import React, { Suspense } from "react";
 import { Row, Col } from "react-bootstrap";
 import { useParams, Link } from "react-router-dom";
-
-import Product from "../product";
 import { useGetProductsQuery } from "../../slices/productsApiSlice";
 import Loader from "../Loader";
-import Message from "../../components/message";
-import Paginate from "../Paginate";
-import ProductCarousel from "../ProductCarousel";
+
+// Lazy load components
+const Product = React.lazy(() => import("../product"));
+const Message = React.lazy(() => import("../../components/message"));
+const Paginate = React.lazy(() => import("../Paginate"));
+const ProductCarousel = React.lazy(() => import("../ProductCarousel"));
 
 const HomeScreen = () => {
   const { pageNumber, keyword } = useParams();
@@ -14,36 +16,39 @@ const HomeScreen = () => {
     keyword,
     pageNumber,
   });
+
   return (
     <>
-      {!keyword ? (
-        <ProductCarousel />
-      ) : (
-        <Link to="/" className="btn btn-light mb-4" />
-      )}
-      {isLoading ? (
-        <Loader />
-      ) : error ? (
-        <Message variant="danger">
-          {error?.data?.message || error.error}
-        </Message>
-      ) : (
-        <>
-          <h1>Latest Products</h1>
-          <Row>
-            {data.products.map((product) => (
-              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                <Product product={product} />
-              </Col>
-            ))}
-          </Row>
-          <Paginate
-            pages={data.pages}
-            page={data.page}
-            keyword={keyword ? keyword : ""}
-          />
-        </>
-      )}
+      <Suspense fallback={<Loader />}>
+        {!keyword ? (
+          <ProductCarousel />
+        ) : (
+          <Link to="/" className="btn btn-light mb-4" />
+        )}
+        {isLoading ? (
+          <Loader />
+        ) : error ? (
+          <Message variant="danger">
+            {error?.data?.message || error.error}
+          </Message>
+        ) : (
+          <>
+            <h1>Latest Products</h1>
+            <Row>
+              {data.products.map((product) => (
+                <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                  <Product product={product} />
+                </Col>
+              ))}
+            </Row>
+            <Paginate
+              pages={data.pages}
+              page={data.page}
+              keyword={keyword ? keyword : ""}
+            />
+          </>
+        )}
+      </Suspense>
     </>
   );
 };
