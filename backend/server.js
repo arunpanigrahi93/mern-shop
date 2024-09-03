67a6573e7d609b3fc4d0c9c5bed89acce068f331
arunpanigrahi93 <arunpanigrahi94@gmail.com>
2024-09-03 20:03:40 +0530

    update comment

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import path from "path";
 import express from "express";
 import dotenv from "dotenv";
-import cors from "cors"; // Import CORS middleware
+import cors from "cors";
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
