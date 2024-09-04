b7c6e92d9c9a0b5a6012a07728ba2bb99efe6624
arunpanigrahi93 <arunpanigrahi94@gmail.com>
2024-09-04 20:17:31 +0530

    added purpose of async handler

--- backend/middleware/asyncHandler.js
+++ backend/middleware/asyncHandler.js
@@ -1,4 +1,11 @@
-const asyncHandler = (fn) => (req, res, next) =>
-  Promise.resolve(fn(req, res, next)).catch(next);
+// asyncHandler is a higher-order function that wraps asynchronous route handlers
+// in Express.js to automatically catch and forward errors to the error-handling middleware.
 
+const asyncHandler = (fn) => (req, res, next) =>
+  // Wrap the asynchronous function in a promise
+  Promise.resolve(fn(req, res, next))
+    // If the promise is rejected (an error occurs), catch it and pass it to the next middleware
+    .catch(next);
+
+// Export the asyncHandler function so it can be used in other parts of the application
 export default asyncHandler;
