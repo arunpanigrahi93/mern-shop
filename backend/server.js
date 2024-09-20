f10a75694408c219bf507f9bfd2df6be5d4e467e
arunpanigrahi93 <arunpanigrahi94@gmail.com>
2024-09-20 19:40:46 +0530

    created functions for production deployment

--- backend/server.js
+++ backend/server.js
@@ -29,10 +29,6 @@ app.use(cors());
 //cookie parser middleware
 app.use(cookieParser());
 
-app.get("/", (req, res) => {
-  res.send("Api is running...");
-});
-
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
@@ -45,6 +41,18 @@ app.get("/api/config/paypal", (req, res) =>
 );
 const __dirname = path.resolve(); //set _dirname to current directory
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+if (process.env.Node_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.join(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("Api is running...");
+  });
+}
 
 // Not Found and Error Handling Middlewares
 app.use(notFound);
